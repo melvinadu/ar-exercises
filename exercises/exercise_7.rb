bc5aa0493e6deac84a7671b0511152b95e9c81c3
Melvin Adu <contactmelvinadu@gmail.com>
2022-02-02 06:48:56 +0000

    check data validation

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,19 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+#Ask the user for a store name (store it in a variable)
+puts "Please enter a store name:"
+store_name = gets.chomp.to_s
+
+#Attempt to create a store with the inputted name but leave out the other fields (annual_revenue, mens_apparel, and womens_apparel)
+
+new_store = Store.create(
+  name: store_name, 
+  )
+
+#Display the error messages provided back from ActiveRecord to the user (one on each line) after you attempt to save/create the record
+
+new_store.errors.full_messages.each do |message| 
+  puts message 
+end
